38da77b5ff5fa9e72dc3505127b1e78d32beb9bf
Marian Schubert <marian.schubert@gmail.com>
2016-10-27 20:55:05 +0200

    Reflect backend changes for /inbox endpoint
    It accepts userId and photoFile parameters (also userId has to be "test"
    for now).

--- app/src/main/java/com/unlessquit/friggr/SendToActivity.java
+++ app/src/main/java/com/unlessquit/friggr/SendToActivity.java
@@ -146,9 +146,9 @@ public class SendToActivity extends AppCompatActivity {
             File sourceFile = new File(paths[0]);
             RequestBody requestBody = new MultipartBody.Builder()
                     .setType(MultipartBody.FORM)
-                    .addFormDataPart("uploaded_file", sourceFile.getName(), RequestBody.create(MEDIA_TYPE_JPG, sourceFile))
-                    .addFormDataPart("user-id", "atest")
-
+                    .addFormDataPart("photoFile", sourceFile.getName(),
+                            RequestBody.create(MEDIA_TYPE_JPG, sourceFile))
+                    .addFormDataPart("userId", "test")
                     .build();
 
             Request request = new Request.Builder()
@@ -158,7 +158,7 @@ public class SendToActivity extends AppCompatActivity {
 
             try (Response response = client.newCall(request).execute()) {
                 if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
-                System.out.println(response.body().string());
+                Log.d("FRIGGR", "Photo has been uploaded");
             } catch (IOException e) {
                 e.printStackTrace();
             }
